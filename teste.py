2abead9cd829bee3db440915c6e6c221cdd65e38
Desktop <rudney_vinicius@hotmaol.com>
2022-10-03 06:56:44 -0300

    Dados da conta

---
+++ teste.py
@@ -0,0 +1,20 @@
+
+
+from wsgiref.validate import validator
+
+
+def cria_conta(numero, titular, saldo, limite):
+    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
+    return conta
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+def saca(conta, valor):
+    conta["saldo"] -= valor #Para criar uma instância, é obrigatório preencher os valores de todos os atributos.#
+
+def extrato(conta):
+    print("saldo {}".format(conta["saldo"])) #O paradigma Orientado a Objetos nos incentiva a agrupar funcionalidades relacionadas em um mesmo lugar. Este é um dos principais problemas.#
+
+
+
