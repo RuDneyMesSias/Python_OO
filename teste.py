1f48b8c57bcbe8397001eb9ed9132f13dbf868a6
Desktop <rudney_vinicius@hotmaol.com>
2022-10-03 08:11:17 -0300

    Classes e Objetos

--- teste.py
+++ teste.py
@@ -1,8 +1,4 @@
 
-
-from wsgiref.validate import validator
-
-
 def cria_conta(numero, titular, saldo, limite):
     conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
     return conta
